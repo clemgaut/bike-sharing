3ec51eb0636a6e2c3c9d0a1ce9a8624afb8cecf0
clemgaut <clemgaut@gmail.com>
2015-01-12 20:26:02 +0100

    Tests different number of tree for random forest.

--- random_forest_analysis.py
+++ random_forest_analysis.py
@@ -10,6 +10,8 @@ from sklearn import metrics
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 import utils
 
 # Index of column with results
@@ -31,15 +33,37 @@ data = np.delete(data, ignore_columns, 1)
 # define custom corer for RMSLE
 rmsle_scorer = metrics.make_scorer(utils.get_RMSLE, greater_is_better=False)
 
+n_trees_list = [10, 50, 100, 200]
+cv_means = []
+cv_stds = []
+
 # build classifier
-clf = RandomForestRegressor(n_estimators=10)
+for n_trees in n_trees_list:
+    clf = RandomForestRegressor(n_estimators=n_trees)
 
-# cross-validation evaluation
-scores = cross_validation.cross_val_score(clf, data, result_vect, cv=5, scoring=rmsle_scorer)
-scores = -scores
+    # cross-validation evaluation
+    scores = cross_validation.cross_val_score(clf, data, result_vect, cv=5, scoring=rmsle_scorer)
+    scores = -scores
+    cv_means.append(np.mean(scores))
+    cv_stds.append(np.std(scores))
 
-print "RMSLE:" + str(np.mean(scores)) + " +- " + str(np.std(scores))
 
+# Plots mean and std depending on number of trees
+plt.subplot(211)
+plt.plot(n_trees_list, cv_means)
+plt.xlabel("Number of trees")
+plt.ylabel("Mean RMSLE")
+
+plt.subplot(212)
+plt.plot(n_trees_list, cv_stds)
+plt.xlabel("Number of trees")
+plt.ylabel("Standard dev RMSLE")
+
+plt.tight_layout()
+plt.show()
+
+# Make predictions according to best result
+clf = RandomForestRegressor(n_estimators=n_trees_list[np.argmax(cv_means)])
 test_data = utils.get_data("test.csv")
 clf.fit(data, result_vect)
 pred_test = clf.predict(test_data)
